16eb87bba5d45b09e5aad2afe844ae3f86a38020
vilius <vilius.kulinas@gmail.com>
2022-02-14 12:57:44 +0000

    Testing CD

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, Depends
-
 from app.config import Settings, get_settings
+from fastapi import APIRouter, Depends
 
 router = APIRouter()
 
@@ -8,7 +7,7 @@ router = APIRouter()
 @router.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {
-        "ping": "pong!",
+        "ping": "pong",
         "environment": settings.environment,
         "testing": settings.testing,
     }
